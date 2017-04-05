b08c4a8c5dfa775ee2ca2785397d85c1ddf9e66a
maazmobin <maazmobin@gmail.com>
2017-04-05 14:06:18 +0500

    library edit

---
+++ Motor_Controller/library/Navigator/Navigator.cpp
@@ -0,0 +1,181 @@
+// Copyright (c) 2014, Solder Spot
+// All rights reserved.
+// See LICENSE.txt in MotorController folder 
+// MODIFIED by Zaid Pirwani
+
+#include "Navigator.h"
+#include <math.h>
+#include <Arduino.h>
+
+//----------------------------------------
+//
+//----------------------------------------
+
+Navigator::Navigator()
+: m_min_dt(nvMS(10))
+{
+    m_init_pose.position.x = 0.0f;
+    m_init_pose.position.y = 0.0f;
+    m_init_pose.heading = 90.0f;  // maaz changed it
+    m_dist_scaler = m_wheel_rl_scaler = m_wheelbase_scaler = 1.0f;
+}
+
+//----------------------------------------
+//
+//----------------------------------------
+
+void Navigator::InitEncoder( nvDistance wheel_diameter, nvDistance wheelbase, uint16_t ticks_per_rev )
+{
+    m_nominal_wheel_diam = wheel_diameter;
+    m_nominal_wheelbase = wheelbase;
+    m_ticks_per_rev = ticks_per_rev;
+    Reset(0);
+}
+
+//----------------------------------------
+//
+//----------------------------------------
+
+void Navigator::Reset( nvTime now )
+{
+    // reset state
+    m_last_ticks_time = now;
+    m_dt = 0;
+    m_lticks = m_rticks = 0;
+    m_pose = m_init_pose;
+    m_heading = nvDegToRad(m_pose.heading);
+    m_speed = nvMM(0.0);
+    m_turn_rate = nvDEGREES(0.0);
+
+    // calc odomerty terms
+    m_effective_wheelbase = m_nominal_wheelbase*m_wheelbase_scaler;
+    m_effective_wheel_diam = m_nominal_wheel_diam*m_dist_scaler;
+    float dist_per_tick = m_ticks_per_rev > 0 ? (m_effective_wheel_diam*M_PI/m_ticks_per_rev) : 0.0f;
+    float invE = 1.0f/m_wheel_rl_scaler;
+    m_rticks_to_dist = dist_per_tick*( 2.0f/(invE+1.0f));
+    m_lticks_to_dist = dist_per_tick*( 2.0f/(m_wheel_rl_scaler + 1.0f));
+}
+
+//----------------------------------------
+//
+//----------------------------------------
+
+bool Navigator::UpdateTicks( int16_t lticks, int16_t rticks, nvTime now )
+{
+    // update delta values
+    m_dt +=  nvDeltaTime( m_last_ticks_time, now );
+    m_lticks += lticks;
+    m_rticks += rticks;
+
+    // remember time for next call
+    m_last_ticks_time = now;
+
+    // see if we have accumulated min time delta 
+    if ( m_dt < m_min_dt )
+    {
+        // no, so wait
+        return false;
+    }
+
+//    m_dt=10;
+//    m_lticks = lticks;
+//    m_rticks = rticks;
+
+    nvDistance sr = ((nvDistance)m_rticks)*m_rticks_to_dist;
+    nvDistance sl = ((nvDistance)m_lticks)*m_lticks_to_dist;
+    nvDistance s  = (sr + sl)*0.5f;
+
+    // calc and update change in heading
+    nvRadians theta = (sr - sl)/m_effective_wheelbase;
+    m_heading = nvClipRadians( m_heading + theta);
+
+    // update velocities (per sec)
+    m_speed = (s*1000.0f)/m_dt;
+    m_turn_rate = (nvRadToDeg(theta)*1000.0f)/m_dt;
+
+    // update pose
+    m_pose.heading = nvRadToDeg(m_heading);
+    m_pose.position.x += s*cos(m_heading);
+    m_pose.position.y += s*sin(m_heading);
+
+    // reset delta values
+    m_dt = 0;
+    m_lticks = 0;
+    m_rticks = 0;
+
+    return true;
+}
+
+//----------------------------------------
+//
+//----------------------------------------
+
+nvPosition Navigator::NewPosition( nvDistance distance )
+{
+    nvPosition pos;
+
+    pos.x = m_pose.position.x + distance*cos(m_heading);
+    pos.y = m_pose.position.y + distance*sin(m_heading);
+
+    return pos;
+}
+
+//----------------------------------------
+//
+//----------------------------------------
+
+nvPosition Navigator::NewPosition( nvDistance x_offset, nvDistance y_offset )
+{
+    nvPosition pos;
+
+    pos.x = m_pose.position.x + x_offset;
+    pos.y = m_pose.position.y + y_offset;
+
+    return pos;
+}
+
+//----------------------------------------
+//
+//----------------------------------------
+
+nvPosition Navigator::NewPositionByHeading( nvPosition &pos, nvHeading heading, nvDistance distance )
+{
+    nvPosition new_pos;
+    nvRadians h = nvDegToRad(nvClipHeading(heading));
+    new_pos.x = pos.x + distance*cos(h);
+    new_pos.y = pos.y + distance*sin(h);
+
+    return new_pos;
+}
+
+//----------------------------------------
+//
+//----------------------------------------
+
+void Navigator::GetTo( nvPosition &pos, nvHeading *heading, nvDistance *distance )
+{
+
+    nvDistance dx = pos.x - m_pose.position.x;
+    nvDistance dy = pos.y - m_pose.position.y;
+
+    *distance = sqrt( dx*dx + dy*dy );
+    *heading = nvClipHeading(nvRadToDeg(atan2(dx, dy)+2*M_PI));
+}
+
+//----------------------------------------
+//
+//----------------------------------------
+
+nvDegrees Navigator::HeadingAdjust( nvHeading target )
+{
+    nvDegrees adjust = nvClipDegrees(target - m_pose.heading);
+    if (adjust < -180.0f)
+    {
+        return adjust + 360.0f;
+    }
+    else if ( adjust > 180.0f )
+    {
+        return adjust - 360.0f;
+    }
+    return adjust; 
+}
---
+++ Motor_Controller/library/Navigator/Navigator.h
@@ -0,0 +1,191 @@
+// Copyright (c) 2014, Solder Spot
+// All rights reserved.
+// See LICENSE.txt in MotorController folder 
+// MODIFIED by Zaid Pirwani
+
+
+#ifndef __NAVIGATOR_H
+#define __NAVIGATOR_H
+
+#include <stdint.h>
+#include <math.h>
+
+
+//----------------------------------------
+// Constants
+//----------------------------------------
+
+#define nvMAX_TIME      0xffffffff
+
+
+//----------------------------------------
+// Base Types
+//----------------------------------------
+
+typedef float           nvCoord;        // millimeters
+typedef float           nvDegrees;      // degrees 
+typedef float           nvRadians;      // radians 
+typedef nvDegrees       nvHeading;      // degrees from North
+typedef float           nvRate;         // change per second
+typedef float           nvDistance;     // millimeters
+typedef uint32_t        nvTime;         // time in milliseconds
+
+//----------------------------------------
+// Helper Macros
+//----------------------------------------
+
+#define nvMM(D)         ((nvDistance)(D))
+#define nvMETERS(D)     ((nvDistance)((D)*1000))
+#define nvMS(MS)        ((nvTime)(MS))
+#define nvSECONDS(S)    ((nvTime)((S)*1000))
+#define nvDEGREES(D)    ((nvDegrees)(D))
+#define nvRADIANSS(R)   ((nvRadians)(R))
+
+#define nvNORTH         nvDEGREES(0)
+#define nvNORTHEAST     nvDEGREES(45)
+#define nvEAST          nvDEGREES(90)
+#define nvSOUTHEAST     nvDEGREES(135)
+#define nvSOUTH         nvDEGREES(180)
+#define nvSOUTHWEST     nvDEGREES(225)
+#define nvWEST          nvDEGREES(270)
+#define nvNORTHWEST     nvDEGREES(315)
+
+//----------------------------------------
+// Helper Functions
+//----------------------------------------
+
+// Time delta function that handles wrapround
+inline nvTime           nvDeltaTime( nvTime last, nvTime now)   { return now >= last ?  now - last : nvMAX_TIME - last + now + 1; }
+
+// radians <-> degrees
+inline nvDegrees        nvRadToDeg( nvRadians rad ) { return (rad*180.0f)/M_PI; }
+inline nvRadians        nvDegToRad( nvDegrees deg ) { return (deg*M_PI)/180.0f; }
+inline nvDegrees        nvClipDegrees( nvDegrees deg ) { return deg - ( ((int32_t)deg/360L)*360.0f); }
+inline nvDegrees        nvClipHeading( nvDegrees deg ) { deg = nvClipDegrees(deg); return deg < 0.00f ? deg + 360.0f : deg; }
+inline nvRadians        nvClipRadians( nvRadians rad ) { rad -= ( ((int32_t)(rad/(2.0f*M_PI)))*2*M_PI); return rad < 0.00f ? rad + 2.0f*M_PI : rad;}
+
+//----------------------------------------
+// nvPosition
+//----------------------------------------
+
+struct nvPosition
+{
+    nvCoord     x;              // mm
+    nvCoord     y;              // mm
+};
+
+//----------------------------------------
+// nvPose
+//----------------------------------------
+
+struct nvPose
+{
+    nvPosition  position;       // mm from (0, 0)
+    nvHeading   heading;        // degrees from North
+};
+
+//----------------------------------------
+// Navigator
+//----------------------------------------
+
+class Navigator
+{
+    public:
+
+        Navigator();
+
+   // initialization
+        void            InitEncoder( nvDistance wheel_diameter, nvDistance wheel_base, uint16_t ticks_per_revolution );
+
+   // service
+        void            Reset( nvTime now );
+        bool            UpdateTicks( int16_t lticks, int16_t rticks, nvTime now );
+		
+   // getters
+        // location
+        nvPose          Pose( void ) { return m_pose; }
+        nvPosition      Position( void ) { return m_pose.position; }
+        nvHeading       Heading( void ) //{return m_pose.heading}
+       //MAAZ EDIT
+        { float angle=m_pose.heading*3.142/180;
+        if(angle>3.142)
+        {
+          angle=angle-3.142;
+          angle=(3.142-angle)*(-1);
+          }
+          return angle; }
+        // END Maaz Edit
+        // movement
+        nvRate          Speed( void ) { return m_speed; }
+        nvRate          TurnRate( void ) { return m_turn_rate; }
+        bool            IsMoving( void ) { return m_speed != 0.0f; }
+        bool            IsTurning( void ) { return m_turn_rate != 0.0f; }
+        bool            InMotion( void ) { return IsMoving() || IsTurning(); }
+
+        // systematic odometry error correction
+        float           DistanceScaler( void) { return m_dist_scaler; }
+        float           WheelRLScaler( void ) { return m_wheel_rl_scaler; }
+        float           WheelbaseScaler( void ) { return m_wheelbase_scaler; }
+
+   // setters
+        // location
+        void            SetStartPose( const nvPose &pose) { m_init_pose.position = pose.position; m_init_pose.heading = nvClipHeading( pose.heading); }
+        void            SetStartPosition( const nvPosition &pos) { m_init_pose.position = pos; }
+        void            SetStartPosition( nvCoord x, nvCoord y) { m_init_pose.position.x = x; m_init_pose.position.y = y; }
+        void            SetStartHeading( nvHeading heading ) { m_init_pose.heading = nvClipHeading(heading); }
+
+        // systematic odometry error correction
+        void            SetWheelRLScaler( float rl_scaler) { m_wheel_rl_scaler = rl_scaler;}
+        void            SetWheelbaseScaler( float scaler ) { m_wheelbase_scaler = scaler;}
+        void            SetDistanceScaler( float scaler) { m_dist_scaler = scaler; }
+
+		// Maaz Edit   
+		void            SetPose( nvCoord my_x , nvCoord my_y , nvHeading adj_angle) {m_pose.position.x=(my_x*10);m_pose.position.y=(my_y*10);m_heading=adj_angle;}
+
+        // config
+        void            SetMinInterval( nvTime min ) { m_min_dt = min; }
+
+   // navigation
+        nvPosition      NewPosition( nvDistance distance ); 
+        nvPosition      NewPositionByHeading( nvPosition &pos, nvHeading heading, nvDistance dist );    
+        nvPosition      NewPositionByHeading( nvHeading heading, nvDistance distance ) { return NewPositionByHeading( m_pose.position, heading, distance); }
+        nvDegrees       HeadingAdjust( nvHeading target ); 
+        nvPosition      NewPosition( nvDistance x_offset, nvDistance y_offset );
+        void            GetTo( nvPosition &pos, nvHeading *heading, nvDistance *distance );
+        nvDistance      LeftWheelTicksToMM( int16_t ticks) { return ((nvDistance)ticks)*m_lticks_to_dist;}
+        nvDistance      RightWheelTicksToMM( int16_t ticks) { return ((nvDistance)ticks)*m_rticks_to_dist;}
+
+    protected:
+
+        // spacial properties
+        nvPose          m_pose;                             // current pose
+        nvRate          m_speed;                            // current mm per second
+        nvRate          m_turn_rate;                        // current degrees per second
+
+        // settings/config                                  
+        nvPose          m_init_pose;                        // starting pose
+        nvTime          m_min_dt;                           // minimum time delta unit
+        uint16_t        m_ticks_per_rev;                    // encoder ticks per wheel revolution
+        nvDistance      m_nominal_wheel_diam;               // nominal wheel diameter
+        nvDistance      m_nominal_wheelbase;                // nominal width of wheel base
+        float           m_wheelbase_scaler;                 // scaling factor to correct nominal wheel base
+        float           m_wheel_rl_scaler;                  // ratio of right and left wheel diameters - used to calculate real diameters
+        float           m_dist_scaler;                      // scaling factor to correct for distance calcs due to effective wheel diamteres 
+
+        // intermediate 
+        float           m_rticks_to_dist;
+        float           m_lticks_to_dist;
+        nvDistance      m_effective_wheelbase; 
+        nvDistance      m_effective_wheel_diam; 
+
+        // state data
+        nvTime          m_last_ticks_time;                  // time of last ticks update
+        nvTime          m_dt;                               // current time delta
+        int16_t         m_lticks;                           // current ticks
+        int16_t         m_rticks;                           // current ticks
+        nvRadians       m_heading;                          // current heading in radians
+
+};
+
+
+#endif // __NAVIGATOR_H
